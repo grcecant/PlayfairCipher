293c64512c663cf44feefb4a39b99dea5aab4ed1
Grace <gracecant888@gmail.com>
2020-10-07 19:15:30 -0400

    basis

---
+++ playfair_cipher.py
@@ -0,0 +1,23 @@
+#------------
+#Playfair Cipher
+#------------
+
+def insert_x(letter_pair):
+    #
+    print()
+
+def which_encode(letter_pair):
+    #
+    print()
+
+def vertical_encode(letter_pair):
+    #
+    print()
+
+def horizontal_encode(letter_pair):
+    #
+    print()
+
+def regular_encode(letter_pair):
+    #
+    print()
