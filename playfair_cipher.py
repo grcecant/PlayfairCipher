fc7a3c554c5f73dbcbe1fdc96fe76857fe5321cb
Grace <gracecant888@gmail.com>
2020-10-09 17:25:20 -0400

    outlining functions, insert x

--- playfair_cipher.py
+++ playfair_cipher.py
@@ -5,30 +5,30 @@
 #imports
 import sys
 
-#variables
-global mode, key, text
-
 #functions
-
 def process_inputs():
     if len(sys.argv) < 4:
         sys.exit()
 
     #args from command line
+    global mode, text, key
     mode = sys.argv[1]
-    text = sys.argv[2]
+    text = sys.argv[2].upper()
     input_key = sys.argv[3]
     key = []
 
     #cut input into chunks of 5 characters
     nums_iterate = [0,5,10,15,20]
     for i in nums_iterate:
-        key.append(input_key[i:i+5])
+        temp = []
+        for j in range(i, i+5):
+            temp.append(input_key[j])
+        key.append(temp)
     print(key)
 
 def insert_x(letter_pair):
-    #
-    print()
+    if first == second:
+        text.insert("X", i+1)
 
 def which_encode(letter_pair):
     #
@@ -46,5 +46,36 @@ def regular_encode(letter_pair):
     #
     print()
 
+#the meaty encode function!
+def playfair_cipher_encode():
+    #iterate through in pairs
+    for i in range(len(text)-1):
+        current_pair = [text[i], text[i+1]]
+        first = current_pair[0]
+        second = current_pair[1]
+
+        print(current_pair)
+        for i in key:
+            if first in i:
+
+        print(key.index(first))
+
+        #if letters are the same, insert an x between them
+        if first == second:
+            insert_x(current_pair)
+
+        #if letters are on the same row, use the letters below them to replace
+
+
+        #if letters are on the same column, use the letters to their right to replace them
+
+
+        #if the letters are different, replace them with the letters on the same row, but in the column of the other letter
+
+
+
 if __name__ == '__main__':
     process_inputs()
+    playfair_cipher_encode()
+    #if mode == "encode":
+        #playfair_cipher_encode()
